f31f49d28d113adb6bea91dce3edc99f0ca8656f
Kranthi1434 <kranthijana1997@gmail.com>
2024-08-02 13:05:17 +0530

    js commit

---
+++ usersData.js
@@ -0,0 +1,24 @@
+let data = [
+  {
+    id: 1,
+    title: "Handmade Fresh Table",
+    price: 687,
+    description: "Andy shoes are designed to keeping in...",
+    category: {
+      id: 1,
+      name: "Others",
+      image: "https://placeimg.com/640/480/any?r=0.591926261873231",
+    },
+  },
+  {
+    id: 2,
+    title: "Handmade Fresh desk",
+    price: 997,
+    description: "For studing and working purpose",
+    category: {
+      id: 2,
+      name: "Others",
+      image: "https://placeimg.com/640/480/any?r=0.591926261873231",
+    },
+  },
+];
